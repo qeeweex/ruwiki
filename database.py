05e1861e0183fde2f9f8712024fda32892e20ac0
unknown <garmashkir09@mail.ru>
2025-06-04 17:56:07 +0300

    убрал ошибки

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
-import hashlib
 import sqlite3
-from article import Article
+import hashlib
+from article import Article, User
 
 
 class Database:
@@ -10,23 +10,19 @@ class Database:
     @staticmethod
     def execute(sql_code: str, params: tuple = ()):
         conn = sqlite3.connect(Database.db_path)
-        
         cursor = conn.cursor()
-        cursor.execute(sql_code, params)
-
+        if params:
+            cursor.execute(sql_code, params)  # Используем execute для параметров
+        else:
+            cursor.executescript(sql_code)    # executescript для DDL-запросов
         conn.commit()
+        conn.close()
 
     @staticmethod
     def create_tables():
         with open(Database.schema_path) as schema_file:
             sql_code = schema_file.read()
-
-            conn = sqlite3.connect(Database.db_path)
-
-            cursor = conn.cursor()
-            cursor.executescript(sql_code)
-
-            conn.commit()
+            Database.execute(sql_code)
 
     @staticmethod
     def update(article_id: int, title: str, content: str, image: str) -> bool:
@@ -56,14 +52,25 @@ class Database:
         return True
 
     @staticmethod
+    def find_user_by_id(user_id: int) -> User | None:
+        users = Database.fetchall('SELECT * FROM users WHERE id = ?', [user_id])
+
+        if not users:
+            return None
+        
+        id, user_name, email, password_hash = users[0]
+        return User(name=user_name, email=email)
+
+    @staticmethod
     def find_article_by_id(article_id: int) -> Article | None:
         articles = Database.fetchall("SELECT * FROM articles WHERE id = ?", [article_id])
 
         if not articles: # if len(articles) == 0
             return None
 
-        id, title, content, image = articles[0]
-        article = Article(id=id, title=title, content=content, image=image)
+        id, title, content, image, author_id = articles[0]
+        author = Database.find_user_by_id(author_id)
+        article = Article(id=id, title=title, content=content, image=image, author=author)
 
         return article
 
@@ -72,9 +79,10 @@ class Database:
         if Database.find_article_by_title(article.title) is not None:
             return False
 
+        author_id = Database.find_user_id_by_name_or_email(article.author.email)
         Database.execute(f"""
-        INSERT INTO articles (title, content, filename) VALUES (?, ?, ?)
-        """, (article.title, article.content, article.image))
+        INSERT INTO articles (title, content, filename, author_id) VALUES (?, ?, ?, ?)
+        """, (article.title, article.content, article.image, author_id))
         return True
 
     @staticmethod
@@ -90,14 +98,16 @@ class Database:
     def get_all_articles():
         articles = []
 
-        for (id, title, content, image) in Database.fetchall(
+        for (id, title, content, image, author_id) in Database.fetchall(
                 "SELECT * FROM articles"):
+            author = Database.find_user_by_id(author_id)
             articles.append(
                 Article(
                     title=title,
                     content=content,
                     image=image,
-                    id=id
+                    id=id,
+                    author=author,
                 )
             )
 
@@ -111,62 +121,61 @@ class Database:
         if not articles:
             return None
         
-        id, title, content, image = articles[0]
-        return Article(title, content, image, id)
+        id, title, content, image, author_id = articles[0]
+        author = Database.find_user_by_id(author_id)
+        return Article(
+            title = title, 
+            content = content, 
+            image = image, 
+            id = id, 
+            author = author
+        )
     
     @staticmethod
     def register_user(user_name, email, password):
-        # 1. Узнать, есть ли пользователи, у которых уже указан такой
-        # никнейм или электронная почта
-        users = Database.fetchall(
-            "SELECT * FROM users WHERE user_name = ? OR email = ?",
-            [user_name, email]
-        )
+        
+        users = Database.fetchall("SELECT * FROM users WHERE user_name = ? OR email = ?", [user_name, email])
+        
+        print(users)
+
         if users:
             return False
-
-        # 2. Получить хэш от пароля, добавить пользователя в БД
-        password_hash = hashlib.md5( password.encode() ).hexdigest()
-        Database.execute(
-            "INSERT INTO users (user_name, email, password_hash) VALUES (?, ?, ?)",
-            [user_name, email, password_hash]
-        )
+        
+        password_hash = hashlib.md5(password.encode()).hexdigest()
+        
+        Database.execute("INSERT INTO users (user_name, email, password_hash) "
+                          "VALUES(?, ?, ?)",
+                        [user_name, email, password_hash])
+        
         return True
+
     
     @staticmethod
-    def get_count_of_users():
-        # Т.к. fetchall возвращает список с кортежом, т.е. [(2,)],
-        # то надо дополнительно написать [0][0]
+    def count_users():
+        # т.к fetchall возвращает список с кортежом
         count = Database.fetchall("SELECT COUNT(*) FROM users")[0][0]
         return count
-    
+        
     @staticmethod
     def can_be_logged_in(user_or_email: str, password: str):
-        # 1. Проверить, что пользователь с таким именем или электронной почтой есть
-        users = Database.fetchall(
-            "SELECT * FROM users WHERE user_name = ? OR email = ?",
-            [user_or_email, user_or_email]
-        )
+        users = Database.fetchall("SELECT * FROM users WHERE user_name = ? OR email = ? ", [user_or_email, password])
+
         if not users:
             return False
         
-        # 2. Берем хэш-пароля заданного пользователя
-        # users = [ (1, "nnn", "nnn@ayndex.ru", "asfksdhfihsiuh523454534jh534kjkhk34j534") ]
         user = users[0]
         real_password_hash = user[3]
 
-        # 3. Сравниваем хэш хранящийся в базе данных и хэш пароля,
-        # который попытались ввести
         password_hash = hashlib.md5( password.encode() ).hexdigest()
+
         if real_password_hash != password_hash:
             return False
         return True
     
-    def find_user_id_by_name_or_email(username_or_email):
-        users = Database.fetchall(
-            "SELECT id FROM urers WHERE user_name = ? OR email = ?", 
-            [username_or_email, username_or_email]
-        )
+    @staticmethod
+    def find_user_id_by_name_or_email(username_on_email):
+        users = Database.fetchall("SELECT id FROM users WHERE user_name = ? OR email = ? ", [username_on_email, username_on_email])
+
         if not users:
             return None
         
